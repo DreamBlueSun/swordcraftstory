38f7d7298c7c9de17d1e49351cdc42ccf0510651
maohangbin <271221198@qq.com>
2021-09-14 11:22:32 +0800

    注册Screen优化

--- src/main/java/com/marisa/swordcraftstory/event/ClientEventHandler.java
+++ src/main/java/com/marisa/swordcraftstory/event/ClientEventHandler.java
@@ -1,24 +1,33 @@
-//package com.marisa.swordcraftstory.event;
-//
-//import com.marisa.swordcraftstory.entity.EntityTypeRegistry;
-//import com.marisa.swordcraftstory.entity.rander.RockLizardRender;
-//import com.marisa.swordcraftstory.entity.rander.SlimeRender;
-//import net.minecraftforge.api.distmarker.Dist;
-//import net.minecraftforge.eventbus.api.SubscribeEvent;
-//import net.minecraftforge.fml.client.registry.RenderingRegistry;
-//import net.minecraftforge.fml.common.Mod;
-//import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-//
-///**
-// * 客户端事件监听处理器
-// */
-//@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-//public class ClientEventHandler {
-//
+package com.marisa.swordcraftstory.event;
+
+import com.marisa.swordcraftstory.gui.container.ContainerTypeRegistry;
+import com.marisa.swordcraftstory.gui.screen.IntensifyEdgeScreen;
+import com.marisa.swordcraftstory.gui.screen.WeaponCollapseScreen;
+import com.marisa.swordcraftstory.gui.screen.WeaponMakeScreen;
+import net.minecraft.client.gui.ScreenManager;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+/**
+ * 客户端事件监听处理器
+ */
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientEventHandler {
+
+    @SubscribeEvent
+    public static void onClientSetupEvent(FMLClientSetupEvent event) {
+        //注册Screen
+        event.enqueueWork(() -> ScreenManager.registerFactory(ContainerTypeRegistry.WEAPON_MAKE_CONTAINER.get(), WeaponMakeScreen::new));
+        event.enqueueWork(() -> ScreenManager.registerFactory(ContainerTypeRegistry.INTENSIFY_EDGE_CONTAINER.get(), IntensifyEdgeScreen::new));
+        event.enqueueWork(() -> ScreenManager.registerFactory(ContainerTypeRegistry.WEAPON_COLLAPSE_CONTAINER.get(), WeaponCollapseScreen::new));
+    }
+
 //    @SubscribeEvent
 //    public static void onClientSetUpEvent(FMLClientSetupEvent event) {
 //        //注册实体的渲染
 //        RenderingRegistry.registerEntityRenderingHandler(EntityTypeRegistry.SLIME.get(), SlimeRender::new);
 //        RenderingRegistry.registerEntityRenderingHandler(EntityTypeRegistry.ROCK_LIZARD.get(), RockLizardRender::new);
 //    }
-//}
+}
