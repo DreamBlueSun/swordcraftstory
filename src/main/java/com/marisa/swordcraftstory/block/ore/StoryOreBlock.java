8cca442f6e41d03153f1e6507516c266dec3a34d
maohangbin <271221198@qq.com>
2021-09-18 21:11:13 +0800

    模组矿石可以被精准采集的稿子正常获取了

--- src/main/java/com/marisa/swordcraftstory/block/ore/StoryOreBlock.java
+++ src/main/java/com/marisa/swordcraftstory/block/ore/StoryOreBlock.java
@@ -1,14 +1,18 @@
 package com.marisa.swordcraftstory.block.ore;
 
+import com.marisa.swordcraftstory.item.ItemRegistry;
 import com.marisa.swordcraftstory.save.StoryPlayerData;
 import com.marisa.swordcraftstory.save.StoryPlayerDataManager;
 import com.marisa.swordcraftstory.util.obj.DropQuality;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.OreBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -30,13 +34,24 @@ public class StoryOreBlock extends OreBlock {
     @Override
     public void spawnAdditionalDrops(BlockState state, ServerWorld worldIn, BlockPos pos, ItemStack stack) {
         if (!worldIn.isRemote && worldIn.getGameRules().getBoolean(GameRules.DO_TILE_DROPS)) {
-            PlayerEntity player = worldIn.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), 8.0D, false);
-            if (player != null) {
-                StoryPlayerData storyPlayerData = StoryPlayerDataManager.get(player.getCachedUniqueIdString());
-                int lv = StoryPlayerDataManager.getLv(storyPlayerData.getXp());
-                NonNullList<ItemStack> list = NonNullList.create();
-                list.add(DropQuality.randomDropQuality(lv));
-                InventoryHelper.dropItems(worldIn, pos, list);
+            if (!stack.isEmpty() && stack.getItem() instanceof PickaxeItem) {
+                //判断精准采集
+                if (EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, stack) > 0) {
+                    //掉落方块本身
+                    NonNullList<ItemStack> list = NonNullList.create();
+                    list.add(ItemRegistry.STORY_ORE_BLOCK.get().getDefaultInstance());
+                    InventoryHelper.dropItems(worldIn, pos, list);
+                } else {
+                    //掉落素材
+                    PlayerEntity player = worldIn.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), 8.0D, false);
+                    if (player != null) {
+                        StoryPlayerData storyPlayerData = StoryPlayerDataManager.get(player.getCachedUniqueIdString());
+                        int lv = StoryPlayerDataManager.getLv(storyPlayerData.getXp());
+                        NonNullList<ItemStack> list = NonNullList.create();
+                        list.add(DropQuality.randomDropQuality(lv));
+                        InventoryHelper.dropItems(worldIn, pos, list);
+                    }
+                }
             }
         }
         super.spawnAdditionalDrops(state, worldIn, pos, stack);
--- src/main/java/com/marisa/swordcraftstory/util/obj/DropQuality.java
+++ src/main/java/com/marisa/swordcraftstory/util/obj/DropQuality.java
@@ -2,7 +2,6 @@ package com.marisa.swordcraftstory.util.obj;
 
 import com.marisa.swordcraftstory.item.ItemRegistry;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,13 +37,13 @@ public class DropQuality {
     private static final int RANK_ORE_1 = 100;
     private static final int RANK_ORE_2 = 80;
     private static final int RANK_ORE_3 = 70;
-    private static final int RANK_RANDOM_MAX = RANK_MATERIAL_0 + RANK_MATERIAL_2 + RANK_MATERIAL_1 +
+    private static final int RANK_RANDOM_MAX = RANK_MATERIAL_0 + RANK_MATERIAL_1 + RANK_MATERIAL_2 +
             RANK_ORE_1 + RANK_ORE_2 + RANK_ORE_3;
 
     public static ItemStack randomDropQuality(int playerLv) {
         int drop = RANK_RANDOM_MAX;
         int r = new Random().nextInt(RANK_RANDOM_MAX) + 1;
-        if (r > (drop - RANK_ORE_3) && playerLv >= 10) {
+        if (r > (drop -= RANK_ORE_3) && playerLv >= 10) {
             return LIST_ORE_RANK_3.get(new Random().nextInt(LIST_ORE_RANK_3.size()));
         }
         if (r > (drop -= RANK_ORE_2) && playerLv >= 5) {
@@ -56,13 +55,11 @@ public class DropQuality {
         if (r > (drop -= RANK_MATERIAL_2)) {
             return ItemRegistry.SHARP_STONE.get().getDefaultInstance();
         }
-        if (r > (drop -= RANK_MATERIAL_1)) {
+        if (r > (drop - RANK_MATERIAL_1)) {
             return ItemRegistry.DOPATIC_STONE.get().getDefaultInstance();
         }
-        if (r > (drop - RANK_MATERIAL_0)) {
-            return ItemRegistry.OMEGA_STONE.get().getDefaultInstance();
-        }
-        return Items.AIR.getDefaultInstance();
+        //RANK_MATERIAL_0
+        return ItemRegistry.OMEGA_STONE.get().getDefaultInstance();
     }
 
 }
