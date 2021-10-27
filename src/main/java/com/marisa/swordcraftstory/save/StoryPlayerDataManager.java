e78f68785eb5064f427ac4a308bf79cdc2e31944
maohangbin <271221198@qq.com>
2021-10-27 23:19:10 +0800

    修复方法：获取升级所需要达到的经验值点

--- src/main/java/com/marisa/swordcraftstory/save/StoryPlayerDataManager.java
+++ src/main/java/com/marisa/swordcraftstory/save/StoryPlayerDataManager.java
@@ -59,7 +59,7 @@ public class StoryPlayerDataManager {
      * @param lv 当前等级
      */
     public static int getLvNextXpPoint(int lv) {
-        if (40 > lv && lv > -1) {
+        if (Story.LV_MAX > lv && lv > -1) {
             return LV_NEED_XP.get(lv + 1);
         } else {
             return 0;
