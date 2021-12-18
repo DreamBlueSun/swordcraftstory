7a9a375d5fd6174a8cc0cb4dfe850cfd437c8915
maohangbin <271221198@qq.com>
2021-12-19 06:50:50 +0800

    属性修改

--- src/main/java/com/marisa/swordcraftstory/Story.java
+++ src/main/java/com/marisa/swordcraftstory/Story.java
@@ -2,23 +2,16 @@ package com.marisa.swordcraftstory;
 
 import com.marisa.swordcraftstory.event.EventHandler;
 import com.marisa.swordcraftstory.item.ItemRegistry;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Story.MOD_ID)
 public class Story {
 
@@ -26,68 +19,30 @@ public class Story {
 
     public static final int LV_MAX = 39;
 
-    // Directly reference a log4j logger.
     public static final Logger LOG = LogManager.getLogger();
 
     public Story() {
 
         ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
         MinecraftForge.EVENT_BUS.register(new EventHandler());
 
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-//        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::attributeFix);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
-        LOG.info("HELLO FROM PREINIT");
-        LOG.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
-
-//    private void doClientStuff(final FMLClientSetupEvent event) {
-//        // do something that can only be done on the client
-//        LOG.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
-//    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo(Story.MOD_ID, "helloworld", () -> {
-            LOG.info("Hello world from the MDK");
-            return "Hello world";
-        });
-    }
-
-    private void processIMC(final InterModProcessEvent event) {
-        // some example code to receive and process InterModComms from other mods
-        LOG.info("Got IMC {}", event.getIMCStream().
-                map(m -> m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-//    @SubscribeEvent
-//    public void onServerStarting(FMLServerStartingEvent event) {
-//        // do something when the server starts
-//        LOG.info("HELLO from server starting");
-//    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOG.info("HELLO from Register Block");
+    /**
+     * 属性修改
+     */
+    private void attributeFix(FMLLoadCompleteEvent event) {
+        for (Attribute attribute : ForgeRegistries.ATTRIBUTES) {
+            if (attribute instanceof RangedAttribute rangedAttribute) {
+                rangedAttribute.minValue = -65535.0F;
+                rangedAttribute.maxValue = 65535.0F;
+            }
         }
     }
+
+
 }
