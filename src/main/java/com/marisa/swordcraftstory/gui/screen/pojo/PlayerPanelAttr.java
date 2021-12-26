8066ec28ce3f85c077812bf477985dbcc5ae5bda
maohangbin <271221198@qq.com>
2021-12-26 13:16:59 +0800

    玩家面板属性显示优化

--- src/main/java/com/marisa/swordcraftstory/gui/screen/pojo/PlayerPanelAttr.java
+++ src/main/java/com/marisa/swordcraftstory/gui/screen/pojo/PlayerPanelAttr.java
@@ -3,11 +3,14 @@ package com.marisa.swordcraftstory.gui.screen.pojo;
 import com.marisa.swordcraftstory.Story;
 import com.marisa.swordcraftstory.save.PlayerData;
 import com.marisa.swordcraftstory.save.util.PlayerDataManager;
+import com.marisa.swordcraftstory.smith.util.SmithNbtUtils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.DiggerItem;
+import net.minecraft.world.item.SwordItem;
 
 /**
  * 玩家面板属性
@@ -20,6 +23,7 @@ public class PlayerPanelAttr {
     private final Component name;
     private final Component hp;
     private final Component atk;
+    private final Component agl;
     private final Component def;
     private final Component phy;
 
@@ -49,8 +53,18 @@ public class PlayerPanelAttr {
                 .append(" ").append(new TranslatableComponent(HP).withStyle(ChatFormatting.GREEN));
         //ATK
         int ATK = (int) player.getAttributeValue(Attributes.ATTACK_DAMAGE);
+        ATK += SmithNbtUtils.getAtk(player.getMainHandItem());
+        if (player.getMainHandItem().getItem() instanceof SwordItem swordItem) {
+            ATK += (int) swordItem.getDamage();
+        } else if (player.getMainHandItem().getItem() instanceof DiggerItem diggerItem) {
+            ATK += (int) diggerItem.getAttackDamage();
+        }
         this.atk = new TranslatableComponent("攻击").withStyle(ChatFormatting.LIGHT_PURPLE)
                 .append(" ").append(new TranslatableComponent(String.valueOf(ATK)).withStyle(ChatFormatting.GREEN));
+        //AGL
+        int AGL = SmithNbtUtils.getAgl(player.getMainHandItem());
+        this.agl = new TranslatableComponent("敏捷").withStyle(ChatFormatting.LIGHT_PURPLE)
+                .append(" ").append(new TranslatableComponent(String.valueOf(AGL)).withStyle(ChatFormatting.GREEN));
         //DEF
         int DEF = player.getArmorValue();
         this.def = new TranslatableComponent("护甲").withStyle(ChatFormatting.LIGHT_PURPLE)
@@ -79,6 +93,10 @@ public class PlayerPanelAttr {
 
     public Component getAtk() {
         return atk;
+    }
+
+    public Component getAgl() {
+        return agl;
     }
 
     public Component getDef() {
